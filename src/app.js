a2f9b51cc220fc7bcfe5cfd11b13976939d64f10
angelaceles <85888474+angelaceles@users.noreply.github.com>
2023-05-19 00:17:22 -0500

    Second commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(require('./controllers/authController'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/login.html'));
+});
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../templates')));
+
+module.exports = app;
